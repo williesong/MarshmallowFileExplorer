3a4d8a3b63e037f69adb5f02cbd19845de3a7bc6
Willie Song <willie.developer@gmail.com>
2017-06-19 13:09:30 +0800

    Support android O.

--- app/src/main/java/com/dp/marshmallowfileexplorer/MainActivity.java
+++ app/src/main/java/com/dp/marshmallowfileexplorer/MainActivity.java
@@ -3,26 +3,12 @@ package com.dp.marshmallowfileexplorer;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    public static final String ACTION_BROWSE_DOCUMENT_ROOT = "android.provider.action.BROWSE_DOCUMENT_ROOT";    // For Marshmallow
-    public static final String ACTION_BROWSE = "android.provider.action.BROWSE";                                // For Nougat
-
-    private static final String DOCUMENT_AUTHORITY = "com.android.externalstorage.documents";
-    private static final String DOCUMENT_ROOT_PRIMARY_EMULATED = "primary";
-
-    private static final String PACKAGE_NAME = "com.android.documentsui";
-    private static final String CLASS_NAME_DOCUMENTS_ACTIVITY = "com.android.documentsui.DocumentsActivity";    // For Marshmallow
-    private static final String CLASS_NAME_FILES_ACTIVITY = "com.android.documentsui.FilesActivity";            // For Nougat
-
-    private static final String EXTRA_SHOW_ADVANCED = "android.content.extra.SHOW_ADVANCED";    // For Nougat
-    private static final String EXTRA_FANCY_FEATURES = "android.content.extra.FANCY";           // For Nougat
-    private static final String EXTRA_SHOW_FILESIZE = "android.content.extra.SHOW_FILESIZE";    // For Nougat
 
     private static final String MEDIA_AUTHORITY = "com.android.providers.media.documents";
     private static final String MEDIA_ROOT_IMAGES = "images_root";
@@ -32,36 +18,51 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
-            try {
-                Intent intent = new Intent(ACTION_BROWSE_DOCUMENT_ROOT);
-                intent.setData(DocumentsContract.buildRootUri(DOCUMENT_AUTHORITY, DOCUMENT_ROOT_PRIMARY_EMULATED));
-                intent.setComponent(new ComponentName(PACKAGE_NAME, CLASS_NAME_DOCUMENTS_ACTIVITY));
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                startActivity(intent);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(this, R.string.open_file_explorer_failed, Toast.LENGTH_LONG).show();
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startActivity(buildBrowseIntentForO());
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                startActivity(buildBrowseIntentForN());
+            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.M) {
+                startActivity(buildBrowseIntentForM());
+            } else {
+                Toast.makeText(this, R.string.unsupported_android_version, Toast.LENGTH_LONG).show();
             }
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            try {
-                Intent intent = new Intent(ACTION_BROWSE);
-                intent.setData(DocumentsContract.buildRootUri(DOCUMENT_AUTHORITY, DOCUMENT_ROOT_PRIMARY_EMULATED));
-                intent.setComponent(new ComponentName(PACKAGE_NAME, CLASS_NAME_FILES_ACTIVITY));
-                intent.putExtra(EXTRA_SHOW_ADVANCED, true);
-                intent.putExtra(EXTRA_FANCY_FEATURES, true);
-                intent.putExtra(EXTRA_SHOW_FILESIZE, true);
-                intent.addCategory(Intent.CATEGORY_DEFAULT);
-                startActivity(intent);
-            } catch (Exception e) {
-                e.printStackTrace();
-                Toast.makeText(this, R.string.open_file_explorer_failed, Toast.LENGTH_LONG).show();
-            }
-        } else {
-            Toast.makeText(this, R.string.unsupported_android_version, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, R.string.open_file_explorer_failed, Toast.LENGTH_LONG).show();
         }
 
         finish();
+    }
+
+    public Intent buildBrowseIntentForM() {
+        Intent intent = new Intent("android.provider.action.BROWSE_DOCUMENT_ROOT");
+        intent.setData(DocumentsContract.buildRootUri("com.android.externalstorage.documents", "primary"));
+        intent.setComponent(new ComponentName("com.android.documentsui", "com.android.documentsui.DocumentsActivity"));
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        return intent;
+    }
+
+    public Intent buildBrowseIntentForN() {
+        Intent intent = new Intent("android.provider.action.BROWSE");
+        intent.setData(DocumentsContract.buildRootUri("com.android.externalstorage.documents", "primary"));
+        intent.setComponent(new ComponentName("com.android.documentsui", "com.android.documentsui.FilesActivity"));
+        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
+        intent.putExtra("android.content.extra.FANCY", true);
+        intent.putExtra("android.content.extra.SHOW_FILESIZE", true);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        return intent;
+    }
+
+    public Intent buildBrowseIntentForO() {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(DocumentsContract.buildRootUri("com.android.externalstorage.documents", "primary"));
+        intent.setComponent(new ComponentName("com.android.documentsui", "com.android.documentsui.files.FilesActivity"));
+        intent.putExtra("android.content.extra.SHOW_ADVANCED", true);
+        intent.putExtra("android.content.extra.FANCY", true);
+        intent.putExtra("android.content.extra.SHOW_FILESIZE", true);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        return intent;
     }
 }
